999906dbdf43a3eafac7b6cff99147075898b3c2
aepstein96 <alexander.epstein96@gmail.com>
2026-04-25 03:21:38 -0400

    fixing dropdown

--- app.py
+++ app.py
@@ -241,13 +241,13 @@ sidebar = html.Div(
         dcc.Dropdown(
             id='sex-dropdown',
             options=[
-                {'label': 'Prefer not to say', 'value': 'All'},
-                {'label': 'Man', 'value': 'Male'},
-                {'label': 'Woman', 'value': 'Female'},
+                {'label': 'Other/Not specified', 'value': 'All'},
+                {'label': 'Male', 'value': 'Male'},
+                {'label': 'Female', 'value': 'Female'},
             ],
             value=None,
             clearable=True,
-            placeholder='Gender (optional)',
+            placeholder='Sex (optional)',
             className="mb-1",
         ),
 
